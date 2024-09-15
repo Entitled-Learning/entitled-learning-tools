07675a4d92491d389977484141eb94ba72de0ee4
Tope Daramola <tope.daram@gmail.com>
2024-09-15 09:37:22 -0500

    pull user claims from authorized user

---
+++ src/EntitledLearning.Enrollment.UI/Models/EntitledLearningUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace EntitledLearning.Enrollment.UI.Models;
+
+public record EntitledLearningUser
+{
+    public string Id { get; set; } = "";
+    public string UserName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+
+    public void SetClaims(IEnumerable<Claim> claims)
+    {
+        foreach (var claim in claims)
+        {
+            switch (claim.Type)
+            {
+                case ClaimTypes.NameIdentifier:
+                    Id = claim.Value;
+                    break;
+                case "emails":
+                    Email = claim.Value;
+                    break;
+                case "name":
+                    UserName = claim.Value;
+                    break;
+                case "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname":
+                    FirstName = claim.Value;
+                    break;
+                case "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname":
+                    LastName = claim.Value;
+                    break;
+            }
+        }
+    }
+}
