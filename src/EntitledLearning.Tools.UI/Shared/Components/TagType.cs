3429863b5d9cb08bc59d5919ad3afce8a83c716f
Tope Daramola <tope.daram@gmail.com>
2025-04-05 13:27:15 -0500

    dashboard updates

---
+++ src/EntitledLearning.Tools.UI/Shared/Components/TagType.cs
@@ -0,0 +1,15 @@
+namespace EntitledLearning.Tools.UI.Shared.Components;
+
+/// <summary>
+/// Represents different types of tags that can be displayed
+/// </summary>
+public enum TagType
+{
+    Beta,
+    New,
+    Updated,
+    Info,
+    Warning,
+    ComingSoon
+}
+
